b1736f81c68107cc51ca56d44e51c6698eb10dba
brianyu18 <brianyu1990@gmail.com>
2018-09-13 00:55:11 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 def speak_to_grandma(ask)
   if ask == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif ask == ask.upcase
     return "NO, NOT SINCE 1938!"
   else
